98c4a6d3ecb4ad4150820cca8bd64091915d4b4c
Jose Humberto Mendez Hernandez <josemendez2204@gmail.com>
2021-12-27 14:55:20 -0400

    logic changes

--- app.js
+++ app.js
@@ -31,40 +31,41 @@ body ("message", "write a message")
   .exists()
   .isLength({min:3})] ,
 (req,res) => {
-  
-  const transporter= nodemailer.createTransport ({
-   service: "gmail",
-    auth: {
-        user:process.env.MAIL_USERNAME,
-        pass:process.env.MAIL_PASSWORD
-    },
-    tls: {
-      // do not fail on invalid certs
-      rejectUnauthorized: false
-  },
-})
-
-const mailOptions= {
-  from: `${req.body.email}`,
-  to: `josemendez2204@gmail.com`,
-  subject: `message from: ${req.body.alias} reason: ${req.body.subject}`,
-  text: `${req.body.message}`
-
-}
-
-
-transporter.sendMail(mailOptions, (error,info) => {
-  if (error) {
-      console.log (error) 
-      res.send('error')
-  } else { 
-      console.log ('email sent' + info.response)
-      res.send('success')
-  }
-})
 
     console.log(req.body)
     const errors = validationResult(req);
+
+    const transporter= nodemailer.createTransport ({
+      service: "gmail",
+       auth: {
+           user:process.env.MAIL_USERNAME,
+           pass:process.env.MAIL_PASSWORD
+       },
+       tls: {
+         // do not fail on invalid certs
+         rejectUnauthorized: false
+     },
+   })
+   
+   const mailOptions= {
+     from: `${req.body.email}`,
+     to: `josemendez2204@gmail.com`,
+     subject: `message from: ${req.body.alias} reason: ${req.body.subject}`,
+     text: `${req.body.message}`
+   
+   }
+   
+   
+   transporter.sendMail(mailOptions, (error,info) => {
+     if (error) {
+         console.log (error) 
+         res.send('error')
+     } else { 
+         console.log ('email sent' + info.response)
+         res.send('success')
+     }
+   })
+   
     if (!errors.isEmpty()) {
       console.log(errors)
       res.status(400).json({ errors: errors.array() });
